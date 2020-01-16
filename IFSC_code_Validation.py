ae1b724aa135fc5c6932afc3dfdca83f0db85d75
Raja <rajasekhart.royal@outlook.com>
2020-01-16 15:16:04 +0530

    changes in print

--- IFSC_code_Validation.py
+++ IFSC_code_Validation.py
@@ -7,15 +7,17 @@ ifsc_codes = []
 with open('/Users/thotarajasekhar/Downloads/44_ifsc.csv', "r") as inf:
     for line in inf:
         ifsc_codes.append(line.strip('\n'))
-    # print(ifsc_codes)
+    print(ifsc_codes)  # comment it while working
 for url in ifsc_codes:
     response = requests.get("https://ifsc.razorpay.com/{}".format(url))
     data = response.json()
-    # print(data)
+    print(data) # comment it while working
     if data == 'Not Found':
         with open('44_NotFound_IFSC.txt', 'a') as the_file:
             the_file.write(url)
             the_file.write("\n")
+        print(url) # comment it while working
     else:
         print(data["IFSC"])
         pass
+
